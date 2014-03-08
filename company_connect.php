d37b710b010898b45e184a29eb6dc221cfc4c3fc
Linzap <soldierzx0705@gmail.com>
2014-03-08 20:27:02 +0800

    init to SQL Serv version
    1. fix bug at home.php (issue)
    2. fix DB encoding to utf-8
    3. fix DateTime data type
    4. company login > done
    5. Permission values at cjcu_lib.php
    6. delete work_type_list colume at  work table

--- company_connect.php
+++ company_connect.php
@@ -8,21 +8,42 @@ include_once("cjcuweb_lib.php");
 $id = sqlsrv_escape(trim($_POST['id']));
 $pw = sqlsrv_escape(trim($_POST['pw']));
 
-$sql = "select * from company where id = '$id'";
-$result = sqlsrv_query($conn,$sql);
-$row = @sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC);
+$sql = "select * from company where id=?";
+// 查詢語句 ? 時，設定其值陣列，因為此 SQL 需要用到外來參數1個 $id
+$params = array($id);
+$options =  array("Scrollable" => SQLSRV_CURSOR_KEYSET);
+$result = sqlsrv_query($conn,$sql,$params,$options);
 
-if($id != null && $pw != null && $row[0] == $id && $row[1] == md5($pw))
-{		
+
+if($result){
+
+	$row = sqlsrv_fetch_array($result, SQLSRV_FETCH_NUMERIC);
+
+	// 查無帳號
+	if(count($row) ==0 ){ error_return(); exit; }
+
+	// 密碼不符或沒有輸入
+	if($id != null && $pw != null && $row[0] == $id && $row[1] == md5($pw)){
 
         $_SESSION['username'] = $id;
-        $_SESSION['level'] = 2;
-        echo '登入成功! 跳轉中，請稍後...';
-        echo '<meta http-equiv=REFRESH CONTENT=1;url=home.php>';
+        $_SESSION['level'] = 4;
+       	success_return();
+	}
+	else error_return(); 
+	
 }
-else
-{
-        echo '登入失敗! 跳轉中，請稍後...';
-        echo '<meta http-equiv=REFRESH CONTENT=1;url=company_login.php>';
+else die(print_r( sqlsrv_errors(), true));
+
+
+function error_return(){
+
+	echo '登入失敗! 跳轉中，請稍後...';
+    echo '<meta http-equiv=REFRESH CONTENT=1;url=company_login.php>';
 }
+
+function success_return(){
+	echo '登入成功! 跳轉中，請稍後...';
+    echo '<meta http-equiv=REFRESH CONTENT=1;url=home.php>';
+}
+
 ?>
